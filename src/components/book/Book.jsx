1c833c632692a46a0e1c8281be94d80f23b349c8
heikagiggle <okaforemmanuellaoluchi@gmail.com>
2024-12-20 00:27:02 +0100

    data file

--- src/components/book/Book.jsx
+++ src/components/book/Book.jsx
@@ -1,58 +1,7 @@
 import "./Book.css";
 import { Link } from "react-router-dom";
 import { useNavigate } from "react-router-dom";
-
-const hairstylesList = [
-  {
-    name: "Hailee Steinfeld",
-    description:
-      "as Violet, a Zaunite orphan, later Piltover enforcer, and Jinx sister",
-    price: "40,000",
-    link: "/book/calendly",
-  },
-  {
-    name: "Kevin Alejandro",
-    description:
-      "as Jayce Talis, a scientist, inventor, and eventual Councilor from Piltover, responsible for the creation of Hextech",
-    price: "40,000",
-    link: "/book/calendly",
-  },
-  {
-    name: "Katie Leung",
-    description:
-      "as Caitlyn Kiramman, a Piltover enforcer, daughter of Councilor Cassandra Kiramman, and former lab assistant to Jayce Talis, who becomes a love interest",
-    price: "40,000",
-    link: "/book/calendly",
-  },
-  {
-    name: "Toks Olagundoye",
-    description:
-      "as Mel Medarda, a council member who takes an interest in Jayce and experiments, later begins a relationship with him",
-    price: "40,000",
-    link: "/book/calendly",
-  },
-  {
-    name: "Harry Lloyd",
-    description:
-      "as Viktor, a disabled partner and former assistant to Councilor Cecil B.",
-    price: "40,000",
-    link: "/book/calendly",
-  },
-  {
-    name: "Jason Spisak",
-    description:
-      "as Silco, a crime lord in Zaun, second adoptive father, and leader of the chem-barons",
-    price: "40,000",
-    link: "/book/calendly",
-  },
-  {
-    name: "Reed Shannon",
-    description:
-      "as Ekko, a childhood friend of Vi and Jinx, and the leader of the revolutionary Zaunite group, the Firelights",
-    price: "40,000",
-    link: "/book/calendly",
-  },
-];
+import { hairstylesList } from "./data";
 
 const Book = () => {
   const navigate = useNavigate();
